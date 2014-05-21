f5ab4f75c57e735ae09fda35e797adea7014682e
Jens Petersen <petersen@fedoraproject.org>
2014-05-21 18:35:38 +0900

    can now determine package in cwd
    next branch
    will probably switch from shelly to HSH

--- FHBuild.hs
+++ FHBuild.hs
@@ -25,46 +25,69 @@ import Prelude hiding (FilePath)
 import Shelly
 import qualified Data.Text as T
 
+import System.Directory (getCurrentDirectory)
 import System.Environment (getArgs, getProgName)
 import System.Exit (ExitCode (..), exitWith)
-import System.IO (Handle, hPutStrLn, stderr)
+import System.FilePath (takeBaseName, takeDirectory)
+import System.IO (hPutStrLn, stderr)
 
 default (T.Text)
 
 main :: IO ()
 main = do
-  args <- getArgs
-  when (length args < 3) $ help stderr >> exitWith (ExitFailure 1)
-  let (com:dist:pkgs) = args
+  (com:dist:pkgs) <- getArgs >>= parseArgs
   shelly $
     mapM_ (command' com (T.pack dist) . T.pack) pkgs
   where
     command' "local" = buildLocal
 --    command' "koji" = buildKoji
     command' _ = undefined
-    
-help :: Handle -> IO ()
-help h = do
+
+commands :: [String]
+commands = ["local" {-, "koji"-}]
+
+parseArgs :: [String] -> IO [String]
+parseArgs [] = help >> return []
+parseArgs [com] | com `elem` commands = do
+  dir <- getCurrentDirectory
+  (pkg, branch) <- determinePkgBranch dir
+  return [com, branch, pkg]
+parseArgs args | (head args `elem` commands && length args >= 3) =
+  return args
+parseArgs _ = help >> return []
+
+determinePkgBranch :: String -> IO (String, String)
+determinePkgBranch dir = do
+  let base = takeBaseName dir
+  if base `elem` ["master", "f20", "f19"]
+    then return (takeBaseName $ takeDirectory dir, base)
+    else do
+    branch <- gitBranch
+    return (base, branch)
+
+help :: IO ()
+help = do
   progName <- getProgName
-  hPutStrLn h $ "Usage: " ++ progName ++ " CMD dist pkg ...\n"
+  hPutStrLn stderr $ "Usage: " ++ progName ++ " CMD dist pkg ...\n"
     ++ "\n"
     ++ "Commands:\n"
     ++ "  local\t\t- build locally\n"
     ++ "  koji\t\t- build in Koji\n"
+  exitWith (ExitFailure 1)
 
-branch :: T.Text -> T.Text
-branch "f21" = "master"
-branch d = d
+dist2branch :: T.Text -> T.Text
+dist2branch "f21" = "master"
+dist2branch d = d
 
 buildLocal :: T.Text -> T.Text -> Sh ()
 buildLocal dist pkg = sub $ do
   d <- test_d $ fromText pkg
-  let brnch = fromText $ branch dist
+  let branch = fromText $ dist2branch dist
   unless d $
-    cmd "fedpkg" "clone" "-b" brnch pkg
+    cmd "fedpkg" "clone" "-b" branch pkg
   cd $ fromText pkg
-  whenM (test_d brnch) $ cd brnch
-  echo $ "== " <> pkg <> ":" <> branch dist <> " =="
+  whenM (test_d branch) $ cd branch
+  echo $ "== " <> pkg <> ":" <> dist2branch dist <> " =="
   when d $
     -- todo check(out) correct branch
     cmd "git" "pull"
@@ -80,3 +103,7 @@ buildLocal dist pkg = sub $ do
   void $ cmd "sudo" "yum" "remove" pkg
   void $ cmd "sudo" "yum" "localinstall" $ "x86_64" </> "*"
   exit 0
+
+-- FIXME
+gitBranch :: IO (String)
+gitBranch = return "f20"
