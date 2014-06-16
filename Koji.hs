cdd94b34038fba492fd0122bc43178303dfc3c92
Jens Petersen <petersen@fedoraproject.org>
2014-06-16 18:58:52 +0900

    cache latest-pkg results; fixes and output improvements from testing
    - use 'words' for haskell-sig packages
    - run gitBranch in pkg dir
    - fix cabalSort with standard 'find'
    - buildDriver caches buildroot dep latest-pkg results
    - nub deps lists to avoid duplicate koji latest-pkg checks

--- Koji.hs
+++ Koji.hs
@@ -17,8 +17,8 @@ module Main where
 
 import Control.Applicative ((<$>))
 import Control.Monad (unless, when)
-import Data.Maybe (fromMaybe, fromJust)
-import Data.List (isPrefixOf, stripPrefix, (\\))
+import Data.Maybe (catMaybes, fromMaybe)
+import Data.List (find, isPrefixOf, nub, stripPrefix, union, (\\))
 
 import System.Directory (doesDirectoryExist, doesFileExist)
 import System.Environment (getArgs, getProgName)
@@ -37,10 +37,11 @@ main = do
   (dist, pkgs) <- getArgs >>= parseArgs
   plan <- mapM (prepPkg dist) pkgs
   sorted <- cabalSort plan
+  putStrLn $ "\nBuilding:" +-+ (unwords $ map fst sorted)
   -- FIXME check plan with "cblrepo -n add"
   -- currently need packagedb-cli.git for pkgdb2
-  hsPkgs <- lines <$> cmd "pkgdb-cli" ["list", "--branch", distBranch dist, "--user", "haskell-sig", "--nameonly"]
-  buildDriver dist (hsPkgs \\ ["ghc"]) plan sorted
+  hsPkgs <- words <$> cmd "pkgdb-cli" ["list", "--branch", distBranch dist, "--user", "haskell-sig", "--nameonly"]
+  buildDriver dist (hsPkgs \\ ["ghc"]) plan [] sorted
 
 help :: IO ()
 help = do
@@ -59,7 +60,7 @@ prepPkg dist pkg = do
   let branch = distBranch dist
       dir = pkg
   dirExists <- doesDirectoryExist dir
-  putStrLn $ "\nPrep =" +-+ pkg ++ ":" ++ branch +-+ "="
+  putStrLn $ "\n== Prepare" +-+ pkg ++ ":" ++ branch +-+ "=="
   unless dirExists $ do
     let anon = ["-a"]
     cmdlog "fedpkg" $ ["clone", "-b", branch, pkg] ++ anon
@@ -71,7 +72,7 @@ prepPkg dist pkg = do
     else do
     cmdAssert "not a Fedora pkg git dir!" "grep" ["-q", "pkgs.fedoraproject.org", wd </> ".git/config"]
     when dirExists $ do
-      dirBranch <- gitBranch
+      dirBranch <- gitBranch wd
       when (dirBranch /= branch) $
         cmd_ "fedpkg" ["--path", wd, "switch-branch", branch]
       diffOrig <- cmd "git" ["-C", wd, "diff", "origin" </> branch]
@@ -82,14 +83,13 @@ prepPkg dist pkg = do
       unless (spec == pkg ++ ".spec") $
         error $ pkg ++ ": inconsistent spec file" +-+ spec
     nvr <- cmd "fedpkg" ["--path", wd, "verrel"]
---    let verrel = removePrefix (pkg ++ "-") nvr
     cmd_ "git" ["--no-pager", "-C", wd, "log", "-1"]
     -- FIXME: handle case of no build
     latest <- (head . words) <$> cmd "koji" ["latest-pkg", "--quiet", dist ++ "-build", pkg]
     if nvr == latest
       then error $ nvr +-+ "already built!"
       else do
-      putStrLn $ latest +-+ "->" +-+ nvr
+      putStrLn $ "\n" ++ latest +-+ "->" +-+ nvr
       let cabal = cabalFile wd pkg nvr
       return (pkg, (nvr, wd, cabal))
 
@@ -113,12 +113,9 @@ nvrVersion nvr =
 
 cabalSort :: [BuildStep] -> IO [BuildStep]
 cabalSort bs = do
-  let hkgMap = map ((\ p -> (removeghc p, p)) . fst) bs
   let cabals = map (\(_,(_,_,c)) -> c) bs
   sorted <- lines <$> cmd "cabal-sort" cabals
-  return $ map (\ p -> (p, find (find p hkgMap) bs)) sorted
-  where
-    find k m = fromJust $ lookup k m
+  return $ catMaybes $ map (\ p -> find (\ (_, (_, _, pth)) -> p == pth) bs) sorted
 
 buildKoji :: String -> String -> String -> FilePath -> IO ()
 buildKoji dist pkg nvr wd = do
@@ -137,29 +134,30 @@ removePrefix :: String -> String -> String
 removePrefix prefix orig =
   fromMaybe (error prefix +-+ "is not prefix of" +-+ orig) $ stripPrefix prefix orig
 
-gitBranch :: IO String
-gitBranch =
-  (removePrefix "* " . head . filter (isPrefixOf "* ") . lines) <$> cmd "git" ["branch"]
+gitBranch :: FilePath -> IO String
+gitBranch wd =
+  (removePrefix "* " . head . filter (isPrefixOf "* ") . lines) <$> cmd "git" ["-C", wd, "branch"]
 
-buildDriver :: String -> [String] -> [BuildStep] -> [BuildStep] -> IO ()
-buildDriver _ _ _ [] = return ()
-buildDriver dist hspkgs plan ((pkg, (nvr, wd, _)):rest) = do
+buildDriver :: String -> [String] -> [BuildStep] -> [String] -> [BuildStep] -> IO ()
+buildDriver _ _ _ _ [] = return ()
+buildDriver dist hspkgs plan buildroot ((pkg, (nvr, wd, _)):rest) = do
+  putStrLn $ "\n== Building" +-+ nvr +-+ "=="
   let spec = wd </> pkg ++ ".spec"
+  -- extract pkg from "pkg = ver"
   deps <- (map (head . words) . lines) <$> cmd "rpmspec" ["-q", "--buildrequires", spec] >>= mapM derefSrcPkg
-  let hdeps = filter (`elem` hspkgs) deps
-  unless (null hdeps) $
-    mapM_ (latestInBuildRoot dist plan) hdeps
+  let hdeps = filter (`elem` hspkgs) $ nub deps
+  depNVRs <- mapM (latestInBuildRoot dist plan buildroot) hdeps
   buildKoji dist pkg nvr wd
-  buildDriver dist hspkgs plan rest
+  buildDriver dist hspkgs plan (union buildroot depNVRs) rest
 
 -- FIXME cache results
-latestInBuildRoot :: String -> [BuildStep] -> String -> IO ()
-latestInBuildRoot dist plan pkg = do
-  let inplan = lookup pkg plan
-  case inplan of
-    Just (nvr, _, _) ->
-      cmd_ "koji" ["wait-repo", dist ++ "-build", "--build", nvr]
-    Nothing -> do
-      latest <- kojiLatestPkg (dist ++ "-build") pkg
-      -- FIXME check pkg git nvr
-      cmd_ "koji" ["wait-repo", dist ++ "-build", "--build", latest]
+latestInBuildRoot :: String -> [BuildStep] -> [String] -> String -> IO String
+latestInBuildRoot dist plan buildroot pkg = do
+  let inPlan = lookup pkg plan
+  nvr <- case inPlan of
+    Just (nvr', _, _) -> return nvr'
+    Nothing -> kojiLatestPkg (dist ++ "-build") pkg
+  -- FIXME check pkg git nvr?
+  unless (nvr `elem` buildroot) $
+    cmdlog "koji" ["wait-repo", dist ++ "-build", "--build", nvr]
+  return nvr
