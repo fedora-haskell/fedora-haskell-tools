daf92289b32440d5f2c37e4ac8181b8d809121fb
Jens Petersen <petersen@fedoraproject.org>
2014-06-27 18:06:12 +0900

    add some cblrepo sanity checks, and removeGhcPrefix

--- Bugzilla.hs
+++ Bugzilla.hs
@@ -16,11 +16,10 @@
 module Main where
 
 import Control.Applicative ((<$>))
--- import Control.Monad (unless, when)
--- import Data.Maybe (catMaybes, fromMaybe)
--- import Data.List (find, isPrefixOf, nub, stripPrefix, union)
-
--- import System.Directory (doesDirectoryExist, doesFileExist)
+import Control.Monad (unless)
+import Data.Char (isLetter)
+import Data.List (isPrefixOf, isSuffixOf)
+import System.Directory (doesFileExist, getCurrentDirectory)
 -- import System.Environment (getArgs, getProgName)
 -- import System.Exit (ExitCode (..), exitWith)
 -- import System.FilePath ((</>))
@@ -38,12 +37,21 @@ data BugState = BugState {
 
 main :: IO ()
 main = do
+  dir <- getCurrentDirectory
+  unless ("cblrepo/f21" `isSuffixOf` dir) $
+    error $ "Not in f21/!" +-+ cblrepoHelp
+  db <- doesFileExist "cblrepo.db"
+  unless db $ error $
+    "No cblrepo.db!" +-+ cblrepoHelp
+  ghc <- shell "cblrepo list | grep '^ghc  '"
+  unless ("ghc  7.6.3" `isPrefixOf` ghc) $
+    error $ "cblrepo.db does not contain ghc-7.6.3:" +-+ ghc
+  -- FIXME check component too?
   bugs <- parseLines . lines <$> bugzilla "query" ["--cc=haskell-devel@lists.fedoraproject.org", "--bug_status=NEW", "--short_desc=is available", "--outputformat=%{id}\n%{bug_status}\n%{summary}\n%{status_whiteboard}"]
   mapM_ checkBug bugs
 
-checkBug :: BugState -> IO ()
-checkBug (BugState bid bst bsum bwh) = do
-  putStrLn bsum
+cblrepoHelp :: String
+cblrepoHelp = "Please run in haskell-sig/cblrepo/f21/ dir.\nGet it with: git clone git://git.fedorahosted.org/git/haskell-sig.git"
 
 bugzilla :: String -> [String] -> IO String
 bugzilla c args = cmd "bugzilla" (c:args)
@@ -55,3 +63,14 @@ parseLines (bid:bst:bsum:[]) = [BugState bid bst bsum ""]
 parseLines (bid:bst:bsum:bwh:rest) =
   BugState bid bst bsum bwh : parseLines rest
 parseLines _ = error "Bad bugzilla query output!"
+
+checkBug :: BugState -> IO ()
+checkBug (BugState bid bst bsum bwh) = do
+  let pkgver = removeSuffix " is available" bsum
+      hkgver = removeGhcPrefix pkgver
+  putStrLn hkgver
+
+removeGhcPrefix :: String -> String
+removeGhcPrefix p@('g':'h':'c':'-':rest) | isLetter $ head rest = rest
+                                        | otherwise = p
+removeGhcPrefix pkg = pkg
