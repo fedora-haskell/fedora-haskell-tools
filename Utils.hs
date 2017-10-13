fd40fab9dbccf939097f2e498a1b20012024da2d
Jens Petersen <petersen@redhat.com>
2017-10-13 23:53:14 +0900

    Utils: koji functions already moved to Koji

--- Utils.hs
+++ Utils.hs
@@ -19,10 +19,9 @@ module Utils where
 #else
 import Control.Applicative ((<$>))
 #endif
-import Data.List (isInfixOf, {-isPrefixOf,-} stripPrefix)
+import Data.List (stripPrefix)
 import Data.Maybe (fromMaybe)
 import System.Exit (ExitCode (..))
-import System.FilePath ((</>))
 import System.Process (readProcess, readProcessWithExitCode, rawSystem)
 
 (+-+) :: String -> String -> String
@@ -106,34 +105,6 @@ sudo c as = cmdlog "sudo" (c:as)
 
 shell :: String -> IO String
 shell c = cmd "sh" ["-c", c]
-
-kojiLatestPkg :: String -> String -> IO String
-kojiLatestPkg dist pkg = do
-  res <- words <$> cmd "koji" ["latest-pkg", "--quiet", dist, pkg]
-  return $ if null res then "" else head res
-
-kojiWaitPkg :: String -> String -> IO ()
-kojiWaitPkg dist pkg = --do
-  cmd_ "koji" ["wait-repo", dist, "--build=" ++ pkg]
-  --putStrLn ""
-
-kojiBuilding :: String -> String -> IO Bool
-kojiBuilding pkg build = do
-  tasks <- lines <$> cmd "koji" ["list-tasks", "--mine", "--quiet"]
-  return $ any (build `isInfixOf`) tasks || any (("/" ++ pkg ++ ":") `isInfixOf`) tasks
-
--- parseKojiTask :: [String] -> Maybe String
--- parseKojiTask [] = Nothing
--- parseKojiTask (l:ls) | "Created task:" `isPrefixOf` l = Just $ removePrefix "Created task: " l
---                       | otherwise = parseKojiTask ls
-
-notInKoji :: String -> FilePath -> String -> String -> IO Bool
-notInKoji branch topdir tag pkg = do
-  latest <- kojiLatestPkg tag pkg
-  local <- cmd "fedpkg" ["--path", topdir </> pkg </> branch, "verrel"]
-  if latest == local
-    then kojiWaitPkg tag latest >> return False
-    else return True
 
 removePrefix :: String -> String -> String
 removePrefix prefix orig =
