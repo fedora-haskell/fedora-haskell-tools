ff743315a81d1f183187f9e19144f2e2df715b70
Jens Petersen <petersen@redhat.com>
2020-11-13 00:06:07 +0800

    Dists: f33/lts16

--- src/Dist.hs
+++ src/Dist.hs
@@ -15,17 +15,17 @@ import SimpleCmdArgs (Parser, argumentWith, auto)
 
 -- | Used for Koji sidetag when needed.
 sidetag :: Dist -> Maybe String
---sidetag (Fedora n) | n >= 32 = Just "build-side-19539"
+--sidetag (Fedora n) | n >= 33 = Just "build-side-19539"
 sidetag _ = Nothing
 
 -- | Maps `Dist` to build tag
 distTag :: Dist -> String
---distTag (Fedora n) | n >= 32 = "f32-build-side-19539"
+--distTag (Fedora n) | n >= 33 = "f33-build-side-19539"
 distTag d = show d ++ "-" ++ fromMaybe "build" (sidetag d)
 
 -- | Maps `Dist` to target tag
 distTarget  :: Dist -> String
---distTarget (Fedora n) | n >= 32 = "f32-build-side-19539"
+--distTarget (Fedora n) | n >= 33 = "f33-build-side-19539"
 distTarget d = show d ++ maybe "" ("-" ++) (sidetag d)
 
 -- | optparse-application DIST arg
@@ -38,8 +38,8 @@ distRemote branch d = "origin/" ++ distBranch branch d
 
 -- | Fedora release being tracked in Hackage Distro data
 hackageRelease :: Dist
-hackageRelease = Fedora 32
+hackageRelease = Fedora 33
 
 -- | Stackage LTS stream major version
 ltsStream :: String
-ltsStream = "lts-15"
+ltsStream = "lts-16"
